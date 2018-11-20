2633f335025c0823e89876820ceb22193b974f82
wkdeo12 <wkdeo12>
2018-11-20 12:16:40 +0900

    테스트코드 작성

---
+++ Sample/sample01.js
@@ -0,0 +1,22 @@
+// let name = "홍길동";
+// let age = 30;
+// console.log(age);
+let name = "Daeyong";
+let len = name.length;
+console.log(len);
+
+let newName = name.replace('Dae','AAA');
+console.log(newName);
+let char = newName.charAt(2);
+console.log(char);
+
+console.log(newName.toUpperCase());
+console.log(newName.toLowerCase());
+var x = 0;
+x += 5;
+console.log(x);
+console.log('hello' + 'world');
+var k = '1'
+console.log(1 + k + 3 + 4);
+console.log(123==1+'2'+3);
+
