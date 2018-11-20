6308e8e8b87b6a76aa718f41a8f3d848310b777f
wkdeo12 <wkdeo12>
2018-11-20 14:09:41 +0900

    test.js추가

---
+++ Sample/Test.js
@@ -0,0 +1,22 @@
+var name = "jdy";
+if(name =="jdy"){
+    console.log("My name is jdy");
+}
+else if (name == 'Jdy'){
+   console.log("My name is Jdy")
+}
+
+for(var i= 0; i<5; i++){
+    console.log("i = %d", i );
+}
+
+let time1 = 100;
+while(time1>100){
+    console.log("Result: %d",time1);
+    time1--;
+}
+let time2 = 100;
+do {
+    console.log("Result:%d",time2);
+    time2--;
+}while(time2>100){console.log("Result:%d",time2);}
